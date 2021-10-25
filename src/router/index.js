670787a8c3568e683b2e958945de5073b8cf3727
Liquor-Lorry <2223485925@qq.com>
2021-10-25 17:19:00 +0800

    项目初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,12 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+// 路由规则
+const routes = [];
 
 const router = createRouter({
+  // 使用hash的路由模式
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/根模块的vuex用法.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>
+      App
+      <!-- 在这里使用根模块state的数据 -->
+      <p>{{ $store.state.username }}</p>
+      <!-- 使用根模块getters的数据 -->
+      <p>{{ $store.getters["newName"] }}</p>
+      <button @click="mutationsFn">click me !</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useStore } from "vuex";
+
+export default {
+  name: "App",
+  setup() {
+    // 使用根模块state的数据
+    const store = useStore();
+    console.log(store.state.username);
+    // 使用根模块getters的数据
+    console.log(store.getters.newName);
+
+    const mutationsFn = () => {
+      // 提交根模块mutations函数
+      // store.commit("updateName");
+
+      // 调用根模块actions函数
+      store.dispatch("updateName");
+    };
+    return {
+      mutationsFn,
+    };
+  },
+};
+</script>
+
+<style lang="less">
+</style>
