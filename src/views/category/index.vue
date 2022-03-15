6ede1e1384d4ea2c897cef76ac00ae70f8ddbc95
LorryHYH <2223485925@qq.com>
2022-03-15 16:59:01 +0800

    分类基础布局搭建完成

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -4,28 +4,101 @@
       <!-- 面包屑 -->
       <XtxBread>
         <xtx-bread-item :to="{ path: '/' }">首页</xtx-bread-item>
-        <xtx-bread-item to="/category/1005001">电器</xtx-bread-item>
-        <xtx-bread-item>空调</xtx-bread-item>
+        <xtx-bread-item v-if="topCategory">{{
+          topCategory.name
+        }}</xtx-bread-item>
       </XtxBread>
       <!-- 轮播图 -->
+      <XtxCarousel :sliders="sliders" style="height: 500px" />
       <!-- 全部分类 -->
+      <div class="sub-list" v-if="topCategory && topCategory.children">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="sub in topCategory.children" :key="sub.id">
+            <a href="javscript:;">
+              <img :src="sub.picture" alt="" />
+              <p>{{ sub.name }}</p>
+            </a>
+          </li>
+        </ul>
+      </div>
       <!-- 推荐商品 -->
     </div>
   </div>
 </template>
 
 <script>
+import { ref, computed } from "vue";
+import { findBanner } from "@/api/home.js";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
 export default {
   name: "TopCategory",
-  // 定义属性
-  data() {
-    return {};
+  setup() {
+    // 轮播图数据
+    const sliders = ref([]);
+    findBanner().then((data) => {
+      sliders.value = data.result;
+    });
+
+    // 面包屑加子分类  ====>去vuex里面拿
+    const route = useRoute();
+    const store = useStore();
+    console.log(route.params.id);
+    console.log(store.state.category.list.find((item) => item.id));
+    const topCategory = computed(() => {
+      let cate = {};
+      // 当前顶级分类 = 根据路由上的ID去vuex中category模块的list中查找
+      // console.log(route.params.id);
+      // console.log(store.state.category.list);
+      return store.state.category.list.find((item) => {
+        return item.id === route.params.id;
+      });
+      if (item) cate = item;
+      return cate;
+    });
+
+    return { sliders, topCategory };
   },
 };
 </script>
 
 <style lang='less' scoped>
 .top-category {
-  height: 300px;
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+      li {
+        width: 168px;
+        height: 160px;
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+          img {
+            width: 100px;
+            height: 100px;
+          }
+          p {
+            line-height: 40px;
+          }
+          &:hover {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
--- src/views/home/Conp/HomeBanner.vue
+++ src/views/home/Conp/HomeBanner.vue
@@ -1,27 +1,27 @@
 <template>
-    <div>
-        <div class="home-banner">
-            <XtxCarousel auto-play :sliders="sliders" />
-        </div>
+  <div>
+    <div class="home-banner">
+      <XtxCarousel auto-play :sliders="sliders" />
     </div>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { findBanner } from "@/api/home.js"
+import { ref } from "vue";
+import { findBanner } from "@/api/home.js";
 export default {
-    name: "HomeBanner",
-    setup() {
-        // 获取轮播图数据
-        const sliders = ref([])
-        findBanner().then(data => {
-            sliders.value = data.result
-            console.log("轮播图数据：",sliders.value)
-        })
+  name: "HomeBanner",
+  setup() {
+    // 获取轮播图数据
+    const sliders = ref([]);
+    findBanner().then((data) => {
+      sliders.value = data.result;
+      // console.log("轮播图数据：",sliders.value)
+    });
 
-        return { sliders }
-    }
-}
+    return { sliders };
+  },
+};
 </script>
 
 <style lang='less' scoped>
@@ -31,16 +31,16 @@ export default {
   position: absolute;
   left: 0;
   top: 0;
-  z-index: 98
+  z-index: 98;
 }
 
 // 覆盖样式
 .xtx-carousel {
-    ::v-deep .carousel-btn.prev {
-        left: 270px;
-    }
-    ::v-deep .carousel-indicator {
-        padding-left: 250px;
-    }
+  ::v-deep .carousel-btn.prev {
+    left: 270px;
+  }
+  ::v-deep .carousel-indicator {
+    padding-left: 250px;
+  }
 }
 </style>
