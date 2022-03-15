190aca4a1de86ef02dee30150cf4e6799b304241
LorryHYH <2223485925@qq.com>
2022-03-15 15:52:15 +0800

    批量注册组件实现

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -3,21 +3,42 @@
 // vue2.0插件写法要素：导出一个对象，有install函数，默认传入了vue构造函数，vue基础之上扩展
 // vue2.0插件写法要素：导出一个对象，有install函数，默认传入了app应用实例，vue基础之上扩展
 
-import XtxSkeleton from "./xtx-Skeleton.vue";
-import XtxCarousel from "./xtx-Carousel.vue";
-import XtxPanel from "./xtx-Panel.vue";
-import XtxBread from "./xtx-Bread.vue";
-
 import defaultImg from "@/assets/images/qrcode.jpg";
+
+// import XtxSkeleton from "./xtx-Skeleton.vue";
+// import XtxCarousel from "./xtx-Carousel.vue";
+// import XtxPanel from "./xtx-Panel.vue";
+// import XtxBread from "./xtx-Bread.vue";
+// import XtxBreadItem from "./xtx-BreadItem.vue";
+
+// 使用 require 提供的函数 context 加载某一个目录下的所有 .vue 后缀的文件。
+// 然后 context 函数会返回一个导入函数 importFn
+// 它有一个属性 keys() 获取所有的文件路径
+// 通过文件路径数组，通过遍历数组，再使用 importFn 根据路径导入组件对象
+// 遍历的同时进行全局注册即可
+
+// context(目录路径，是否加载子目录，加载文件的匹配正则)
+const importFn = require.context("./", false, /\.vue$/);
+// console.log("--", importFn.keys());
 
 export default {
   install(app) {
+    // 根据keys批量注册
+    importFn.keys().forEach((path) => {
+      // 导入组件
+      // console.log("....", importFn(path).default);
+      const component = importFn(path).default;
+      //进行注册
+      app.component(component.name, component);
+    });
+
     // 在app上进行扩展，app提供 component directive 函数
     // 如果要挂载原型 app.config.globalProperties 方式
-    app.component(XtxSkeleton.name, XtxSkeleton);
-    app.component(XtxCarousel.name, XtxCarousel);
-    app.component(XtxPanel.name, XtxPanel);
-    app.component(XtxBread.name, XtxBread);
+    // app.component(XtxSkeleton.name, XtxSkeleton);
+    // app.component(XtxCarousel.name, XtxCarousel);
+    // app.component(XtxPanel.name, XtxPanel);
+    // app.component(XtxBread.name, XtxBread);
+    // app.component(XtxBreadItem.name, XtxBreadItem);
 
     // 定义指令
     defineDirective(app);
